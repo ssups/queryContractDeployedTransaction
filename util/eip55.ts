10fa17f94d0d8b4e7cf0e5a4f5e584d50a6a2ae6
ssups <shalstlqruki@gmail.com>
2023-09-07 12:40:48 +0900

    fix: eip55

--- util/eip55.ts
+++ util/eip55.ts
@@ -4,15 +4,14 @@ export function getChecksumAddress(address: string) {
   address = address.replace("0x", "").toLowerCase();
   if (address.length !== 40) throw new Error("wrong address");
 
-  const hashed = keccak256(address);
-  let hexHashed = "";
-  hashed.forEach((el) => {
-    hexHashed += el.toString(16);
-  });
+  const hashed = keccak256(address).reduce(
+    (acc, cur) => (acc += cur.toString(16).padStart(2, "0")),
+    "0x"
+  );
 
   let ret = "0x";
   for (let i = 0; i < address.length; i++) {
-    if (parseInt(hexHashed[i], 16) >= 8) ret += address[i].toUpperCase();
+    if (parseInt(hashed[i], 16) >= 8) ret += address[i].toUpperCase();
     else ret += address[i];
   }
 
